59eb2059d8114192115eb9cceeb4f369246083ad
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-06-19 13:37:25 -0600

    updating styling to details page

--- app/components/Views/ProductDetails.js
+++ app/components/Views/ProductDetails.js
@@ -22,13 +22,13 @@ export default class ProductDetails extends Component {
           <Image
             source={require('../../../app/images/oils/lemon.jpeg')}
             style={styles.productDetailsHeadlineImage} />
-          <Text>{this.props.product} Details Page</Text>
+          <Text style={styles.productDetailsHeadlineText}>{this.props.product} Details Page</Text>
         </View>
         <View style={styles.productDetailsBody}>
-          <Text>Scientific Name: </Text>
-          <Text>Synonyms: </Text>
-          <Text>Properties: </Text>
-          <Text>Description: </Text>
+          <Text style={styles.productDetailsBodyText}>Scientific Name: </Text>
+          <Text style={styles.productDetailsBodyText}>Synonyms: </Text>
+          <Text style={styles.productDetailsBodyText}>Properties: </Text>
+          <Text style={styles.productDetailsBodyText}>Description: </Text>
         </View>
       </View>
     )
@@ -39,22 +39,41 @@ const styles = StyleSheet.create({
   productDetailsContainer: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'grey'
+    backgroundColor: 'grey',
+    paddingTop: 100
   },
   productDetailsHeadline: {
     flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'center',
     height: 100
   },
   productDetailsHeadlineImage: {
-    width: 50,
-    height: 50,
-    borderRadius: 25
+    width: 100,
+    height: 100,
+    borderRadius: 50
+  },
+  productDetailsHeadlineText: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    paddingLeft: 10,
+    fontSize: 16
+  },
+  productDetailsBody: {
+    backgroundColor: 'white',
+    marginTop: 20,
+    width: '100%',
+    paddingLeft: 10,
+    paddingRight: 10
   },
   productHeadlineText: {
     fontSize: 50,
     fontWeight: 'bold',
     alignSelf: 'center'
+  },
+  productDetailsBodyText: {
+    paddingTop: 10,
+    flexWrap: 'wrap'
   }
 })
